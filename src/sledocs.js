9fd6a7dd01a7ce4dd32eaffcb2689bb6271cddd7
Tom Peres <tomp@wix.com>
2019-08-10 10:13:33 +0300

    lint

--- src/sledocs.js
+++ src/sledocs.js
@@ -6,17 +6,17 @@ function parseItem(item) {
 function parseSlackBody(raw) {
   return raw.split('&').reduce((result, curr) => ({
     ...result,
-    ...(parseItem(curr)) 
-  }), {})
+    ...(parseItem(curr)),
+  }), {});
 }
 
 module.exports = async (event) => {
-  const { text } = parseSlackBody(event.body)
+  const {text} = parseSlackBody(event.body);
   return {
     statusCode: 200,
     body: `searched! 💪 ${text}`,
-  }
-}
+  };
+};
 
 // post to slack
 // curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
--- test/slack-sledocs.spec.js
+++ test/slack-sledocs.spec.js
@@ -1,14 +1,15 @@
 const sledocs = require('../src/sledocs');
 
+// eslint-disable-next-line max-len
 const mockBody = ({text}) => `token=1nAm2GHEXEyAklFzPU0zd65a&team_id=TM18CJRBK&team_domain=tomp-personal&channel_id=CM7KFBKBP&channel_name=general&user_id=UM7K8QNAC&user_name=tomp&command=%2Fsledocs&text=${text}&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTM18CJRBK%2F722004961236%2Fz6klTADxeDeM6DRXnwlFHovG&trigger_id=711032798515.715284637393.1474955833da7dc35f9a68bf7bcabcbf`;
 
 describe('sledocs', () => {
   it('should return text from slack', async () => {
     const text = 'wham-bam';
     const {statusCode, body} = await sledocs({
-      body: mockBody({text})
-    })
-    expect(statusCode).toBe(200)
-    expect(body).toBe(`searched! 💪 ${text}`)
+      body: mockBody({text}),
+    });
+    expect(statusCode).toBe(200);
+    expect(body).toBe(`searched! 💪 ${text}`);
   });
 });
